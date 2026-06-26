8e8d36d68903c6a1b3133fcc0c2d3fc16dea8e7b
roymaxfriend-dev <roymaxfriend@gmail.com>
2026-06-26 19:16:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+window.addEventListener("scroll",function(){
 
+const header=document.querySelector(".header");
+
+if(window.scrollY>50){
+
+header.style.background="#08140d";
+
+}else{
+
+header.style.background="rgba(0,0,0,.45)";
+
+}
+
+});
